f3ce6345f60c4ae7e6f2efaff5f662a722a4fea4
dmitryalexeevi4 <dmitryalexeevi4@yandex.ru>
2020-03-16 14:56:07 +0300

    Selenium HW4 (first commit)

--- src/test/java/com/github/dmitryalexeevi4/SeleniumHW4.java
+++ src/test/java/com/github/dmitryalexeevi4/SeleniumHW4.java
@@ -3,9 +3,10 @@ package com.github.dmitryalexeevi4;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.*;
 
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 import static com.github.dmitryalexeevi4.TestUtils.*;
@@ -19,28 +20,97 @@ public class SeleniumHW4 {
     }
 
     @Test()
-    public void dragAndDropPageTest() {
-        LOG.info("Выполнение теста страницы Drag'n'Drop...");
+    public void test() {
         webDriver.get("https://savkk.github.io/selenium-practice/");
-        webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        webDriver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
-        clickButton("dnd");
-        WebElement firstBlackSquare = webDriver.findElement(By.xpath("//div[@id = 'content']//div[1]"));
-        LOG.info(firstBlackSquare.getAttribute("class"));
-        WebElement redSquare = webDriver.findElement(By.xpath("//div[@id = 'content']//div[2]"));
-        LOG.info(redSquare.getAttribute("class"));
-        WebElement secBlackSquare = webDriver.findElement(By.xpath("//div[@id = 'content']//div[3]"));
-        LOG.info(secBlackSquare.getAttribute("class"));
+        LOG.info("Выполнение действий на странице Button...");
+        clickButton("button");
+        clickButton("first");
+        WebElement expectedResult = webDriver.findElement(By.xpath("//label[text()[contains(.,'Excellent')]]"));
+        Assert.assertTrue(expectedResult.isDisplayed());
+        WebElement clickMeTooButton = webDriver.findElement(By.className("button-primary"));
+        Assert.assertTrue(clickMeTooButton.isDisplayed());
+        clickMeTooButton.click();
+        assertLink();
 
-        Actions builder = new Actions(webDriver);
+        LOG.info("Выполнение действий на странице Checkboxes and Radio...");
+        clickButton("checkbox");
+        clickButton("one");
+        clickButton("go");
+        assertResult("result", "one");
+        clickButton("radio_one");
+        clickButton("radio_go");
+        assertResult("radio_result", "radio_one");
+        assertLink();
 
-        builder.dragAndDrop(redSquare, firstBlackSquare).perform();
-        builder.dragAndDrop(redSquare, secBlackSquare).perform();
+        LOG.info("Выполнение действий на странице Select...");
+        clickButton("select");
+        TestUtils.chooseSelectOption("hero");
+        TestUtils.chooseSelectMultipleOptions("languages");
+        clickButton("go");
+        assertSelectedOptionsResult("languages");
+        assertLink();
+
+        LOG.info("Выполнение действий на странице Form...");
+        clickButton("form");
+        fillingReqTextFields();
+        clickSubmitButton();
+        assertLink();
+
+        LOG.info("Выполнение действий на странице IFrame...");
+        clickButton("iframe");
+        webDriver.switchTo().frame(0);
+        WebElement labelText = webDriver.findElement(By.xpath("//label[@id = 'code']"));
+        String text = labelText.getText();
+        String code = text.substring(text.lastIndexOf(" ")).trim();
+        webDriver.switchTo().parentFrame();
+        WebElement textField = webDriver.findElement(By.xpath("//input[@name = 'code']"));
+        textField.sendKeys(code);
+        clickVerifyButton();
+        assertLink();
+
+        LOG.info("Выполнение действий на странице Prompt, Alert and Confirm...");
+        clickButton("alerts");
+        webDriver.findElement(By.className("get")).click();
+        Alert getPassAlert = webDriver.switchTo().alert();
+        String alertMessage = getPassAlert.getText();
+        LOG.info(alertMessage);
+        String password = alertMessage.substring(alertMessage.lastIndexOf(" ")).trim();
+        LOG.info(password);
+        getPassAlert.accept();
+        webDriver.findElement(By.className("set")).click();
+        Alert setPassAlert = webDriver.switchTo().alert();
+        setPassAlert.sendKeys(password);
+        setPassAlert.accept();
+        assertTextAndLink();
+        Alert areYouSureAlert = webDriver.switchTo().alert();
+        areYouSureAlert.accept();
+
+        LOG.info("Выполнение действий на странице Table...");
+        clickButton("table");
+        chooseMultipleCustomers();
+        clickDeleteButton();
+        fillingTextFields();
+        clickAddButton();
+        assertLink();
+
+        LOG.info("Список cookies: ");
+        Set<Cookie> cookies = webDriver.manage().getCookies();
+        for (Cookie cookie : cookies) {
+            LOG.info(cookie.getName());
+        }
+
+        LOG.info("Проверка cookies на значение \"done\"...");
+        for (Cookie cookie : cookies) {
+            Assert.assertEquals(cookie.getValue(), "done");
+        }
+        LOG.info("Успешно");
     }
 
     @AfterClass
     public void webDriverQuit() {
         LOG.info("Закрытие ChromeDriver");
-        //webDriver.quit();
+        webDriver.quit();
     }
 }
